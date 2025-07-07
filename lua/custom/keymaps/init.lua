300ba5ab14b644eb467f842c8ecbbd2bef6562e2
Brandon <badamsva@gmail.com>
2025-07-07 02:46:08 -0400

    feat: add keymaps

---
+++ lua/custom/keymaps/init.lua
@@ -0,0 +1,12 @@
+local Keymaps = {}
+
+function Keymaps.setup()
+  -- Golang errors
+  vim.keymap.set('i', '<C-e>', 'if err != nil {<Esc>o}<Esc>O', { desc = 'Format error', noremap = true, silent = true })
+  -- Diagnostic keymaps
+  vim.keymap.set('n', '<leader>e', vim.diagnostic.setloclist, { desc = 'Open diagnostic [E]rrors list' })
+
+  vim.keymap.set('n', '<C-h>', vim.lsp.buf.hover, bufopts)
+end
+
+return Keymaps
